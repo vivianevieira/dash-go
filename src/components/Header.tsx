3fdd67e6ca8507950e946a86d3eb26da5043c12e
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-13 15:04:50 -0700

    Added avatar to header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -47,6 +47,38 @@ export function Header() {
 
         <Icon as={RiSearchLine} fontSize="20" />
 
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+
+      >
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.600"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Viviane Vieira</Text>
+            <Text color="gray.300" fontSize="small">
+              viviane.kodama@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Vivine Vieira" src="https://github.com/vivanevieira.png"/>
+
+        </Flex>
       </Flex>
     </Flex>
   );
