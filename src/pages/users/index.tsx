e8a28be78944d5749dd172a1e6b73c6f49126caa
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-14 13:47:42 -0700

    Styled Users page.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@chakra-ui/button";
+import { Checkbox } from "@chakra-ui/checkbox";
 import Icon from "@chakra-ui/icon";
-import { Box, Flex, Heading } from "@chakra-ui/layout";
-import { RiFileAddLine } from "react-icons/ri";
+import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -22,13 +24,95 @@ export default function UserList() {
               size="sm"
               fontSize="sm"
               colorScheme="pink"
-              leftIcon={<Icon as={RiFileAddLine} />}
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
             >
               Add new
             </Button>
           </Flex>
 
-
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>User</Th>
+                <Th>Sign-up date</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Viviane Vieira</Text>
+                    <Text fontSize="sm" color="gray.300">viviane.kodama@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>April 14, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Viviane Vieira</Text>
+                    <Text fontSize="sm" color="gray.300">viviane.kodama@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>April 14, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Viviane Vieira</Text>
+                    <Text fontSize="sm" color="gray.300">viviane.kodama@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>April 14, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
         </Box>
       </Flex>
     </Box>
