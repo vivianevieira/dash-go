05a9fb6b5b1e139d0b7214d8c4aa574b0715c257
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-16 16:45:17 -0700

    Created separate files for components from Pagination.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,7 +5,7 @@ import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header/index";
-import { Pagination } from "../../components/Pagination";
+import { Pagination } from "../../components/Pagination/index";
 import { Sidebar } from "../../components/Sidebar/index";
 
 export default function UserList() {
