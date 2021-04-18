731a78ec27c151a67288002f56639807b74ca9b6
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-17 17:05:52 -0700

    Used Chakra BreakPointValue to make Header component responsive.

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Viviane Vieira</Text>
-        <Text color="gray.300" fontSize="small">
-          viviane.kodama@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Viviane Vieira</Text>
+          <Text color="gray.300" fontSize="small">
+            viviane.kodama@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Vivine Vieira" src="https://github.com/vivanevieira.png"/>
 
