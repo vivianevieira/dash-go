2d67078100f1338c221ad991347f0dd173a42cb5
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-19 09:56:54 -0700

    Created a separate comonent for ActiveLink.

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,25 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { cloneElement, ReactElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement
+}
+export function ActiveLink({ children, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  let isActive = false;
+
+  if (asPath === rest.href || asPath === rest.as) {
+    isActive = true;
+  }
+
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? 'pink.400' : 'gray.50'
+      })}
+    </Link>
+  );
+}
