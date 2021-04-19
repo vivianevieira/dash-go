5696b2a67b1349ba6f9e6dec1c0913908b1ffd21
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-19 10:05:16 -0700

    Created a separate component for ActiveLink.

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -3,17 +3,23 @@ import { useRouter } from "next/router";
 import { cloneElement, ReactElement } from "react";
 
 interface ActiveLinkProps extends LinkProps {
-  children: ReactElement
+  children: ReactElement;
+  shouldMatchExactHref?: boolean;
 }
-export function ActiveLink({ children, ...rest }: ActiveLinkProps) {
+export function ActiveLink({ children, shouldMatchExactHref = false, ...rest }: ActiveLinkProps) {
   const { asPath } = useRouter();
 
   let isActive = false;
 
-  if (asPath === rest.href || asPath === rest.as) {
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
     isActive = true;
   }
 
+  if (!shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) ||
+    asPath.startsWith(String(rest.as)))) {
+      isActive = true;
+    }
 
   return (
     <Link {...rest}>
