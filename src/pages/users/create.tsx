61e8eb4f9b1bec98e59ad935dea25b554abbd637
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-29 16:14:29 -0700

    used React Query Mutation to post new user to api.

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import  { useMutation } from 'react-query';
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header/index";
 import { Sidebar } from "../../components/Sidebar/index";
+import { api } from "../../services/api";
 
 type CreateUserFormData = {
   name: string;
@@ -26,13 +28,24 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date()
+      }
+    })
+
+    return response.data.user;
+  })
+
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(values);
+    await createUser.mutateAsync(values);
   }
 
   const { errors } = formState;
